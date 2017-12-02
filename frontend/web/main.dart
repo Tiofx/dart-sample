60f5a664c81b594786e511bafa2cdbbdf6818981
Aleksey <korshvi@mail.ru>
2017-12-02 21:35:04 +0300

    Changes in base files

--- frontend/web/main.dart
+++ frontend/web/main.dart
@@ -1,4 +1,6 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+import 'package:frontend/app_component.dart';
 //import 'package:angular2/router.dart'
 //    show
 //    HashLocationStrategy,
@@ -8,10 +10,9 @@ import 'package:angular/angular.dart';
 import 'package:frontend/app_component.dart';
 
 void main() {
-  bootstrap(AppComponent,
-//      [
-//    ROUTER_PROVIDERS,
-//    provide(LocationStrategy, {useClass: HashLocationStrategy})
-//  ]
-  );
+  bootstrap(AppComponent, [
+    ROUTER_PROVIDERS,
+    // Remove next line in production
+    provide(LocationStrategy, useClass: HashLocationStrategy),
+  ]);
 }

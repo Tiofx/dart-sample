57204e167b0ed96baf073075fdc73fb83f238bf5
Tiofx <ioracle2718@gmail.com>
2017-12-02 18:13:47 +0300

    [front : add] table_service for get data

---
+++ frontend/lib/src/technicalTask/table/table_service.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:angular/angular.dart';
+import 'package:frontend/src/technicalTask/item/item.dart';
+import 'package:http/http.dart';
+import 'package:http/browser_client.dart';
+
+@Injectable()
+class TableService {
+  static const _authority = 'localhost:8080';
+  static const _itemsPath = 'api/technical_task/items';
+  final Client _http;
+
+  TableService(this._http);
+
+  Future<List<Item>> getItems(int pageNumber,
+      int perPage,
+      [int offset = 0]) async {
+    try {
+      final response = await _http.get(
+//          '$_itemsUrl/?page=$pageNumber&per_page=$perPage&offset=$offset'
+          new Uri.http(
+              _authority,
+              _itemsPath,
+              {
+                'page': '$pageNumber',
+                'per_page': '$perPage',
+                'offset': '$offset',
+              }
+          )
+      );
+
+      print(_extractData(response));
+
+      final items = _extractData(response)
+          .map((value) => new Item.fromJson(value))
+          .toList();
+      return items;
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  dynamic _extractData(Response response) => JSON.decode(response.body);
+
+  Exception _handleError(dynamic e) {
+    print(e);
+    return new Exception('Server error; cause: $e');
+  }
+}

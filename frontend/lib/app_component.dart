a74f604434f018c8727c991d88c6af408c928760
Aleksey <korshvi@mail.ru>
2017-12-02 21:32:35 +0300

    Associate app_component wtih project pages

--- frontend/lib/app_component.dart
+++ frontend/lib/app_component.dart
@@ -1,18 +1,42 @@
 import 'package:angular/angular.dart';
-import 'package:angular_components/angular_components.dart';
+import 'package:angular_router/angular_router.dart';
 
-import 'src/todo_list/todo_list_component.dart';
-
-// AngularDart info: https://webdev.dartlang.org/angular
-// Components info: https://webdev.dartlang.org/components
+import 'package:frontend/src/base/project_service.dart';
+import 'package:frontend/src/projects/dashboard_component.dart';
+import 'package:frontend/src/projects/details/project_detail_component.dart';
+import 'package:frontend/src/projects/stages/projects_component.dart';
 
 @Component(
   selector: 'my-app',
+  template: '''
+  <h1>{{title}}</h1>
+  <nav>
+    <a [routerLink]="['Dashboard']">Dashboard</a>
+    <a [routerLink]="['Projects']">Projects</a>
+  </nav>
+  <router-outlet></router-outlet>
+''',
+
   styleUrls: const ['app_component.css'],
-  templateUrl: 'app_component.html',
-  directives: const [materialDirectives, TodoListComponent],
-  providers: const [materialProviders],
+  directives: const [ROUTER_DIRECTIVES],
+  providers: const [ProjectService],
 )
+
+@RouteConfig(const [
+  const Redirect(path: '/', redirectTo: const ['Dashboard']),
+  const Route(
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+  ),
+  const Route(
+    path: '/detail/:id',
+    name: 'ProjectDetail',
+    component: ProjectDetailComponent,
+  ),
+  const Route(path: '/projects', name: 'Projects', component: ProjectsComponent),
+])
+
 class AppComponent {
-  // Nothing here yet. All logic is in TodoListComponent.
+  final title = 'Timetables';
 }

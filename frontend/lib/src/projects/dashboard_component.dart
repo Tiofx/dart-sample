173add52240c30874895603c25cefbf42be722b4
Aleksey <korshvi@mail.ru>
2017-12-02 21:33:36 +0300

    Dashboard page and styles

---
+++ frontend/lib/src/projects/dashboard_component.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+import 'package:frontend/src/base/project.dart';
+import 'package:frontend/src/base/project_service.dart';
+
+
+
+@Component(
+  selector: 'my-dashboard',
+  templateUrl: 'dashboard_component.html',
+  styleUrls: const ['dashboard_component.css'],
+  directives: const [CORE_DIRECTIVES, ROUTER_DIRECTIVES],
+)
+
+class DashboardComponent implements OnInit {
+  List<Project> projects;
+
+  final ProjectService _projectService;
+
+  DashboardComponent(this._projectService);
+
+  Future<Null> ngOnInit() async {
+    projects = (await _projectService.getProjects()).skip(1).take(4).toList();
+  }
+}

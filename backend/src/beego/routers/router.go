94cee5355bc92186b10633e2b641ab79661b7afd
Tiofx <ioracle2718@gmail.com>
2017-12-02 18:08:50 +0300

    [add] routing to the controller

--- backend/src/beego/routers/router.go
+++ backend/src/beego/routers/router.go
@@ -2,8 +2,26 @@ package routers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 	"beego/controllers"
+	"beego/controllers/technicalTask"
 )
 
 func init() {
+	//beego.SetStaticPath("/styles.css","views/build/web/styles.css")
+	//beego.SetStaticPath("/main.dart.js","views/build/web/main.dart.js")
+
+	namespace := beego.NewNamespace("/api",
+		beego.NSBefore(func(context *context.Context) {
+			context.ResponseWriter.Header().Add("Access-Control-Allow-Origin", "*")
+		}),
+		beego.NSRouter("/", &controllers.MainController{}),
+		beego.NSNamespace("/technical_task",
+			beego.NSNamespace("/items",
+				beego.NSInclude(&technicalTask.ItemsController{}),
+			),
+		),
+	)
+
+	beego.AddNamespace(namespace)
 }

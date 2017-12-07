e8ab8ce523fa054d68115c0d643142f58cab1aa9
Aleksey <korshvi@mail.ru>
2017-12-07 07:26:26 +0300

    Adding styles for table_component and ui fixes

--- frontend/lib/src/technicalTask/table/table_component.dart
+++ frontend/lib/src/technicalTask/table/table_component.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
 import 'dart:convert';
+
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
-import 'package:frontend/src/baseEntry/user.dart';
+
+import 'package:angular_router/angular_router.dart';
 import 'package:frontend/src/pageSelector/page_selector_component.dart';
 import 'package:frontend/src/technicalTask/item/add/item_form_component.dart';
+
 import 'package:frontend/src/technicalTask/item/item.dart';
 import 'package:frontend/src/technicalTask/item/item_component.dart';
 import 'package:frontend/src/technicalTask/item/item_service.dart';
@@ -12,6 +16,7 @@ import 'package:frontend/src/technicalTask/item/item_service.dart';
 @Component(
     selector: 'technical-task-table',
     templateUrl: 'table_component.html',
+    styleUrls: const ['table_component.css'],
     directives: const[
       CORE_DIRECTIVES,
       materialDirectives,
@@ -21,7 +26,10 @@ import 'package:frontend/src/technicalTask/item/item_service.dart';
     ],
     providers: const [ItemService]
 )
+
 class TableComponent implements OnInit {
+  Item item;
+  final Router _router;
   final ItemService service;
 
   @Input()
@@ -35,7 +43,7 @@ class TableComponent implements OnInit {
 
   List<Item> items = [];
 
-  TableComponent(this.service);
+  TableComponent(this.service, this._router);
 
   @override
   ngOnInit() async {
@@ -47,17 +55,23 @@ class TableComponent implements OnInit {
     updateItems();
   }
 
-  addItem(Item item) async {
-    var newItem = await service.addItem(item);
-    print("new item: ${newItem.toString()}");
+  Future<Null> goAdd() => _router.navigate(['AddItem']);
 
-    updateItems();
-  }
+//  addItem(Item item) async {
+//    var newItem = await service.addItem(item);
+//    print("new item: ${newItem.toString()}");
+//
+//    updateItems();
+//  }
+
+  Future<Null> goEditItem(Item item) => _router.navigate([
+      'EditItem',
+      {'id': item.number.toString()}
+    ]);
 
   editItem(Item item) async {
     var result = await service.editItem(item);
     print(result);
-
     updateItems();
   }
 
